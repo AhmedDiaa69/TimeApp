c6e1f7421d2499493e0b003e0fb0c8e3c7866d70
AhmedDiaa69 <diaaa625@gmail.com>
2025-08-05 16:04:19 +0300

    Added a border to the fav time when it doesn't have time in it

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,13 +6,16 @@ export default function Card({ title, time, date, timeZone, className }) {
       <h1 id="title" className="text-2xl font-bold mb-4 text-center">
         {title}
       </h1>
-      <div className={`p-4 rounded-lg flex flex-row items-center justify-between w-full min-h-24 bg-(--color-surface) shadow-lg`}>
+      <div
+        className={`p-4 rounded-lg flex flex-row items-center justify-between w-full min-h-24 bg-(--color-surface) shadow-lg ${
+          time ? "" : "border-dashed border-4 border-(--color-border)"
+        }`}
+      >
         <div className="text-2xl font-mono text-(--color-primary)">
           {time ? time : "00:00:00"}
         </div>
         <div className="text-sm text-(--color-text-muted) mt-1">
-          {date ? date : "00-00-00"},{" "}
-          {timeZone ? timeZone : "UTC"}
+          {date ? date : "00-00-00"}, {timeZone ? timeZone : "UTC"}
         </div>
       </div>
     </div>
