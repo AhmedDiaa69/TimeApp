0f8bbd05b34a78cc1bfe157a47ea845ed7f17abf
AhmedDiaa69 <diaaa625@gmail.com>
2025-08-15 15:12:51 +0300

    refactor FavTime component to Add more that one city

--- src/components/fav Time/FavTime.jsx
+++ src/components/fav Time/FavTime.jsx
@@ -1,13 +1,41 @@
 import Card from "../card/Card";
+import { useState, useEffect } from "react";
 
 export default function FavTime({ favCityTime, favCityDate, favCityData }) {
+  const [favCities, setFavCities] = useState([]);
+
+  useEffect(() => {
+    if (!favCityData) return;
+
+    let duplicates = favCities.some(
+      (city) =>
+        favCityData.city === city.data.city &&
+        favCityData.province === city.data.province
+    );
+
+    if (!duplicates) {
+      setFavCities((prev) => [
+        ...prev,
+        { time: favCityTime, date: favCityDate, data: favCityData },
+      ]);
+    } else {
+      console.log("City already exists in favorites or no data provided.");
+    }
+  }, [favCityData, favCityDate, favCityTime]);
+
   return (
     <Card
-      title={favCityData ? `Favorite City: ${favCityData.city}, ${favCityData.country}` : "Your Favorite Time"}
+      title={
+        favCities.length > 0
+          ? `Favorite City: ${favCities[favCities.length - 1].data.city}, ${
+              favCities[favCities.length - 1].data.country
+            }`
+          : "Your Favorite Time"
+      }
       className="fav-time-card"
-      time={favCityTime}
-      date={favCityDate}
-      timeZone={favCityData?.timezone}
+      time={favCities[favCities.length - 1]?.time}
+      date={favCities[favCities.length - 1]?.date}
+      timeZone={favCities[favCities.length - 1]?.data?.timezone}
     />
   );
 }
