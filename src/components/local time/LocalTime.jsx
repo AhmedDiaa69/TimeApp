dd94f3e7d2bd80c4f0deafd27a82e3c14402b519
AhmedDiaa69 <diaaa625@gmail.com>
2025-08-19 00:40:28 +0300

    Changed the defalult value if there is no weather displayed

--- src/components/local time/LocalTime.jsx
+++ src/components/local time/LocalTime.jsx
@@ -82,15 +82,15 @@ export default function LocalTime() {
               console.error(error);
               setWeather({
                 ...weather,
-                humidity: "",
-                temp: "",
-                temp_min: "",
-                temp_max: "",
-                wind_speed: "",
-                visibility: "",
-                feels_like: "",
-                description: "",
-                pressure: "",
+                humidity: 0,
+                temp: 0,
+                temp_min: 0,
+                temp_max: 0,
+                wind_speed: 0,
+                visibility: 0,
+                feels_like: 0,
+                description: 0,
+                pressure: 0,
                 icon: "03n",
               });
             });
@@ -99,15 +99,15 @@ export default function LocalTime() {
           console.error(error);
           setWeather({
             ...weather,
-            humidity: "",
-            temp: "",
-            temp_min: "",
-            temp_max: "",
-            wind_speed: "",
-            visibility: "",
-            feels_like: "",
-            description: "",
-            pressure: "",
+            humidity: 0,
+            temp: 0,
+            temp_min: 0,
+            temp_max: 0,
+            wind_speed: 0,
+            visibility: 0,
+            feels_like: 0,
+            description: 0,
+            pressure: 0,
             icon: "03n",
           });
         });
@@ -117,7 +117,7 @@ export default function LocalTime() {
 
     const interval = setInterval(updateWeather, 300000);
 
-    return () => clearInterval(interval);;
+    return () => clearInterval(interval);
   }, [city]);
 
   return (
